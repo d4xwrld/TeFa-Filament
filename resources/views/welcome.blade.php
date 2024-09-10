192dc706f31c1f5ad58bee9c128872737063164b
d4xwrld <reyzcraft87@gmail.com>
2024-09-10 09:25:17 +0700

    UI Improvements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,18 +63,21 @@
             </svg>
             <!-- Product Content Section -->
             <div style="background-color: #3D3B8E;">
-                <div class="container mx-auto p-8 flex-col">
-                    <h2 class="text-4xl font-extrabold text-white text-center mb-8">Our Products</h2>
-                    <div class="flex flex-col md:flex-row">
+                <div class="container mx-auto p-8">
+                    <h2 class="text-4xl font-extrabold text-white text-center mb-8" data-aos="zoom-out-up"
+                        data-aos-duration="1000">Our Products
+                    </h2>
+                    <div
+                        class="flex flex-col md:flex-row items-stretch justify-center space-y-4 md:space-y-0 md:space-x-4">
                         @foreach ($posts as $post)
-                            <div class="w-full md:w-1/2" data-aos="fade-up" data-aos-duration="1000">
+                            <div class="w-full md:w-1/3" data-aos="fade-up" data-aos-duration="1000">
                                 <div
-                                    class="max-w-sm h-full bg-white border border-gray-200 rounded-md shadow dark:bg-gray-800 dark:border-gray-700 mb-4">
+                                    class="flex flex-col h-full bg-white border border-gray-200 rounded-md shadow dark:bg-gray-800 dark:border-gray-700">
                                     <a href="#">
                                         <img class="rounded-t-lg" src="{{ url('storage/' . $post->image_url) }}"
                                             alt="" />
                                     </a>
-                                    <div class="p-5">
+                                    <div class="p-5 flex-grow">
                                         <a href="#">
                                             <h5
                                                 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -84,11 +87,12 @@
                                                     {{ $post->category->name }}
                                                 </span>
                                             </h5>
-
                                         </a>
                                         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
                                             {{ $post->description }}
                                         </p>
+                                    </div>
+                                    <div class="p-5 mt-auto">
                                         <a href="{{ route('posts.show', ['slug' => $post->slug]) }}"
                                             class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                             See Review
@@ -106,6 +110,7 @@
                     </div>
                 </div>
             </div>
+
         </div>
     </section>
 </body>
